3204d9eee78c129d747488c654d6b7d5e89a2d4f
juanmamelo <juanmanuelmelo95@gmail.com>
2024-12-05 17:26:53 -0300

    Tercera Preentrega

--- inicio/forms.py
+++ inicio/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class CrearProducto(forms.Form):
-    nombre = forms.CharField(max_length=100)
-    precio = forms.CharField()
-    stock = forms.IntegerField()
+    nombre = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Nombre...'}))
+    precio = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Precio...'}))
+    stock = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Stock...'}))
     
 class BuscarProducto(forms.Form):
     nombre = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'placeholder': 'Nombre...'})) 
